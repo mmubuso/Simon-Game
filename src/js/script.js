a844634bfb07a02fde0c720bf95541e3b7c9c18b
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-13 12:32:06 -0400

    add object that tracks round

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 // Javascript arry
 
 //Variables 
-
+let colorArr = [1,2,3,4]
 
 
 //Functions
@@ -9,6 +9,11 @@
 
 //EventListernes
 //Displays the instructions for the game when clicked
-$('.content .btn').eq(1).on('click',function(){
-    swal("Instructions!", "Think fast... SIMON says, Chase my flashing lights and sounds The challenge is to repeat the ever-increasing random signals that SIMON generates.There are three game variations you can play, and you can even set the level of difficulty you want. In any case, you are sure to enjoy hours of challenging fun with SIMON", "info");
+$('.content .btn').eq(1).on('click', function () {
+    swal({
+        title: "Good job!",
+        text: "Think fast... SIMON says, Chase my flashing lights and sounds The challenge is to repeat the ever-increasing random signals that SIMON generates.There are three game variations you can play, and you can even set the level of difficulty you want. In any case, you are sure to enjoy hours of challenging fun with SIMON",
+        icon: "success",
+        button: "I Understand",
+    })
 })
