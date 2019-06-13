cffd2e25f43a86a32b2a2629f99dff029e7abe8c
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-13 12:10:24 -0400

    add event listerner to show instructions

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+// Javascript arry
+
+//Variables 
+
+
+
+//Functions
+
+
+//EventListernes
+//Displays the instructions for the game when clicked
+$('.content .btn').eq(1).on('click',function(){
+    swal("Instructions!", "Think fast... SIMON says, Chase my flashing lights and sounds The challenge is to repeat the ever-increasing random signals that SIMON generates.There are three game variations you can play, and you can even set the level of difficulty you want. In any case, you are sure to enjoy hours of challenging fun with SIMON", "info");
+})
