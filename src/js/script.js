d9c6417750c4b6c5f1c106564610c64e408a35a9
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-13 12:41:38 -0400

    made addone and updateDisplay one function

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,20 @@
 
 //Variables 
 let colorArr = [1,2,3,4]
+//Buttons to be clicked on
+let greenPiece = $('#green')
+let redPiece = $('red')
+let yellowPiece = $('#yellow')
+let bluePiece = $('#blue')
 
+//Objects
+let levelTrack = {
+    score: 0,
+    addOne: function(){
+        this.score++
+        $('.scoreKeeper p').text(this.score)
+    }
+}
 
 //Functions
 
@@ -12,7 +25,7 @@ let colorArr = [1,2,3,4]
 $('.content .btn').eq(1).on('click', function () {
     swal({
         title: "Good job!",
-        text: "Think fast... SIMON says, Chase my flashing lights and sounds The challenge is to repeat the ever-increasing random signals that SIMON generates.There are three game variations you can play, and you can even set the level of difficulty you want. In any case, you are sure to enjoy hours of challenging fun with SIMON",
+        text: "Think fast... SIMON says, Chase my flashing lights and sounds \nThe challenge is to repeat the ever-increasing random signals that SIMON generates.\nThere are three game variations you can play, and you can even set the level of difficulty you want. In any case, you are sure to enjoy hours of challenging fun with SIMON",
         icon: "success",
         button: "I Understand",
     })
